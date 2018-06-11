1e3ce7d8eeb282becb3163c23187b0bc04f31135
Pedro Fausto <pedro.fausto@hotmail.com>
2018-06-11 16:06:06 -0300

    ajuste de layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  validates :price, numericality: { greater_than: 0 }
+  validates :name, presence: true
+  validates :cod, presence: true
+  validates :price, presence: true
+
 end
