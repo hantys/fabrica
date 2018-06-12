a392d89c6278225df766884841be137f96bc2324
Pedro Fausto <pedro.fausto@hotmail.com>
2018-06-12 00:23:57 -0300

    seed

--- Rakefile
+++ Rakefile
@@ -4,3 +4,9 @@
 require_relative 'config/application'
 
 Rails.application.load_tasks
+namespace :db do
+    task :seed => :environment do
+        env_seed_file = File.join(Rails.root, "db", "seeds", "#{Rails.env}.rb")
+        load(env_seed_file) if File.exist?(env_seed_file)
+    end
+end
---
+++ db/seeds/production.rb
@@ -0,0 +1,19 @@
+puts "### Cidades e Estados"
+load "db/imports/states_cities.rb"
+
+
+puts '#Funcionatios'
+
+state = State.find(rand(1..27))
+Employee.create name: 'Pedro Fausto', email: 'pedro.fausto@hotmail.com', cpf: FFaker::IdentificationBR.pretty_cpf, phone1:FFaker::PhoneNumberBR.mobile_phone_number, state: state, city: state.cities.sample(1).last, office: FFaker::JobBR.title, cep: FFaker::AddressBR.zip_code, number: rand(100..9999)
+
+state = State.find(rand(1..27))
+Employee.create name: 'Vitor Alves', email: 'vitor@vitor.com', cpf: FFaker::IdentificationBR.pretty_cpf, phone1:FFaker::PhoneNumberBR.mobile_phone_number, state: state, city: state.cities.sample(1).last, office: FFaker::JobBR.title, cep: FFaker::AddressBR.zip_code, number: rand(100..9999)
+
+
+puts '#Usuario'
+User.create!(username: 'pedro', email: 'pedro.fausto@hotmail.com', password: 'pi854098', employee: Employee.first, roles: 'admin')
+User.create!(username: 'vitor', email: 'vitor@vitor.com', password: '12345678', employee: Employee.last, roles: 'admin')
+
+puts "### Itens do orcamento"
+load "db/imports/budget_items.rb"
