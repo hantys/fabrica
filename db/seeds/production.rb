9626d653e5e44dab031dedf82cf18e8721a24322
Pedro Fausto <pedro.fausto@hotmail.com>
2018-06-12 10:14:01 -0300

    seeed

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -18,24 +18,15 @@
 # puts "### Itens do orcamento"
 # load "db/imports/budget_items.rb"
 
-puts '#Tipo de entrega'
-DeliveryOption.create! name: 'Entrega na loja'
-DeliveryOption.create! name: 'Retirada em mãos'
+# puts '#Tipo de entrega'
+# DeliveryOption.create! name: 'Entrega na loja'
+# DeliveryOption.create! name: 'Retirada em mãos'
 
-puts '#Tipo de pagamento'
-TypeOfPayment.create! name: 'Boleto bancário'
-TypeOfPayment.create! name: 'Cartão de crédito'
-TypeOfPayment.create! name: 'A vista'
-TypeOfPayment.create! name: 'Cheque'
-TypeOfPayment.create! name: 'Débito'
-TypeOfPayment.create! name: 'Transferência'
-TypeOfPayment.create! name: 'Depósito'
-
-Budget.all.each do |budget|
-  if budget.delivery_option_id.present?
-    budget.delivery_option_id = budget.delivery_option_id+1
-  end
-  if budget.type_of_payment_id.present?
-    budget.type_of_payment_id = budget.type_of_payment_id+1
-  end
-end
+# puts '#Tipo de pagamento'
+# TypeOfPayment.create! name: 'Boleto bancário'
+# TypeOfPayment.create! name: 'Cartão de crédito'
+# TypeOfPayment.create! name: 'A vista'
+# TypeOfPayment.create! name: 'Cheque'
+# TypeOfPayment.create! name: 'Débito'
+# TypeOfPayment.create! name: 'Transferência'
+# TypeOfPayment.create! name: 'Depósito'
