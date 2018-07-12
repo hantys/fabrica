9c411ce425e9e282a1c7cc67e2f473596c062c41
Pedro Fausto <pedro.fausto@hotmail.com>
2018-07-12 10:08:13 -0300

    teste 2

--- app/models/out_of_stock.rb
+++ app/models/out_of_stock.rb
@@ -15,35 +15,33 @@ class OutOfStock < ApplicationRecord
   private
 
     def update_qnt_product
-      ActiveRecord::Base.transaction do
-        product = self.product
+      product = self.product
 
-        @stocks = product.stock_final_products.where('amount_out > 0').order(:id)
-        @product_stocks = []
-        @i = 0
-        @calc_cost = 0
-        @qnt = self.qnt
-        while @qnt > 0  do
-          stock = @stocks[@i]
-          ItemProductStock.transaction do
-            if @qnt <= stock.amount_out
-              @product_stocks << ItemProductStock.create!(stock_final_product_id: stock.id, product_id: product.id, qnt: @qnt)
-              @qnt = 0
-            else
-              rest = @qnt - stock.amount_out
-              @product_stocks << ItemProductStock.create!(stock_final_product_id: stock.id, product_id: product.id, qnt: (@qnt - rest))
-              @qnt = rest
-            end
+      @stocks = product.stock_final_products.where('amount_out > 0').order(:id)
+      @product_stocks = []
+      @i = 0
+      @calc_cost = 0
+      @qnt = self.qnt
+      while @qnt > 0  do
+        stock = @stocks[@i]
+        ItemProductStock.transaction do
+          if @qnt <= stock.amount_out
+            @product_stocks << ItemProductStock.create!(stock_final_product_id: stock.id, product_id: product.id, qnt: @qnt)
+            @qnt = 0
+          else
+            rest = @qnt - stock.amount_out
+            @product_stocks << ItemProductStock.create!(stock_final_product_id: stock.id, product_id: product.id, qnt: (@qnt - rest))
+            @qnt = rest
           end
-          @i +=1
         end
-
-        @product_stocks.each do |item|
-          stock = item.stock_final_product
-          @calc_cost = @calc_cost+((stock.cost*item.qnt)/stock.amount)
-        end
-        self.update! cost: @calc_cost
+        @i +=1
       end
+
+      @product_stocks.each do |item|
+        stock = item.stock_final_product
+        @calc_cost = @calc_cost+((stock.cost*item.qnt)/stock.amount)
+      end
+      self.update! cost: @calc_cost
     end
 
 end
