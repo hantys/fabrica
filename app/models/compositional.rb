1538ffba3829fa63402a685d2ec9a1fc031f5807
Pedro Fausto <pedro.fausto@hotmail.com>
2018-08-01 18:09:29 -0300

    correcao

--- app/models/compositional.rb
+++ app/models/compositional.rb
@@ -2,8 +2,7 @@ class Compositional < ApplicationRecord
   acts_as_paranoid
 
   belongs_to :raw_material, -> { with_deleted } , touch: true
-  belongs_to :composition, -> { with_deleted } , touch: true
-
+  belongs_to :composition, -> { with_deleted } , touch: true, optional: true
   has_paper_trail
 
   # validates :raw_material_id, presence: true
