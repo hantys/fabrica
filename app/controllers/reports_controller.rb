d7be08eb57584e698cc3b57acdcfc554fbd9afc8
Pedro Fausto <pedro.fausto@hotmail.com>
2018-08-07 18:18:25 -0300

    campo obs nos orcamentos

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,7 +10,7 @@ class ReportsController < ApplicationController
         order_by = params[:q][:s]
       end
     end
-    @products = @q.result.left_joins(budget_products: [:budget]).where(budgets: {status: 2}).select("sum(budget_products.qnt) as qnt_pedidos", "(sum(budget_products.qnt) - products.qnt) as para_produzir", "count(budget_products.product_id) as total_produto", "products.*").group("budget_products.product_id", "products.id", "products.name", "products.cod").having("(sum(budget_products.qnt) - products.qnt) > ?", 0).order(order_by)
+    @products = @q.result.left_joins(budget_products: [:budget]).where(budgets: {status: 2}).select("array_agg(budgets.id) as budgets_ids, sum(budget_products.qnt) as qnt_pedidos", "(sum(budget_products.qnt) - products.qnt) as para_produzir", "count(budget_products.product_id) as total_produto", "products.*").group("budget_products.product_id", "products.id", "products.name", "products.cod").having("(sum(budget_products.qnt) - products.qnt) > ?", 0).order(order_by)
   end
 
 end
