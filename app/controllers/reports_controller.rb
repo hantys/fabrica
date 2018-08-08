b31a860ca38dd019979685f645a0eef50b38887c
Pedro Fausto <pedro.fausto@hotmail.com>
2018-08-08 15:23:13 -0300

    ajuste na busca de relatorio de producao

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,15 +2,37 @@ class ReportsController < ApplicationController
   authorize_resource class: false
 
   def daily_production
-    @q = Product.ransack(params[:q])
 
     order_by = "products.name asc"
+    @employee = nil
+    @client = nil
+    merge_where = {status: 2}
+
     if params[:q]
       if params[:q][:s]
         order_by = params[:q][:s]
       end
+      @employee = params[:q][:budget_products_budget_employee_id_in].drop(1)
+      @client = params[:q][:budget_products_budget_client_id_in].drop(1)
+      params[:q][:budget_products_budget_employee_id_in] = ''
+      params[:q][:budget_products_budget_client_id_in] = ''
     end
-    @products = @q.result.left_joins(budget_products: [:budget]).where(budgets: {status: 2}).select("array_agg(budgets.id) as budgets_ids, sum(budget_products.qnt) as qnt_pedidos", "(sum(budget_products.qnt) - products.qnt) as para_produzir", "count(budget_products.product_id) as total_produto", "products.*").group("budget_products.product_id", "products.id", "products.name", "products.cod").having("(sum(budget_products.qnt) - products.qnt) > ?", 0).order(order_by)
+
+    @q = Product.ransack(params[:q])
+    if params[:q]
+      params[:q][:budget_products_budget_employee_id_in] = @employee
+      params[:q][:budget_products_budget_client_id_in] = @client
+    end
+
+    if @client.present?
+      merge_where[:client_id] = @client
+    end
+    if @employee.present?
+      merge_where[:employee_id] = @employee
+    end
+
+
+    @products = @q.result.left_joins(budget_products: [:budget]).where(budgets: merge_where).select("array_agg(distinct(budgets.id)) as budgets_ids, sum(budget_products.qnt) as qnt_pedidos", "(sum(budget_products.qnt) - products.qnt) as para_produzir", "count(budget_products.product_id) as total_produto", "products.*").group("budget_products.product_id", "products.id", "products.name", "products.cod").having("(sum(budget_products.qnt) - products.qnt) > ?", 0).order(order_by)
   end
 
 end
